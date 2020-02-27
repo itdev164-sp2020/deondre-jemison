29497cc54dc5424e86417ec76a28730652c9f2ed
Deondre Jemison <jemisodd@gmatc.matc.edu>
2020-02-27 14:38:06 -0600

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `Blarg Blog`,
     description: `ITDEV-164 Gatsby Blog`,
     author: `Deondre Jemison`,
     contact: {
